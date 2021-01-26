38b3e00febb15ea9ad830653978d5d08ad6e1705
Simay Gökalp <simay.gokalp@boun.edu.tr>
2021-01-26 03:09:26 +0300

    Final HW3

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+struct Node
+{
+	int i;
+	Node* pLeft;
+	Node* pRight;
+	Node(int iIn) : i(iIn), pLeft(nullptr), pRight(nullptr) {}
+};
+
+//Creation of the Tree
+void InsertToTree(Node*& pRoot, Node* pNew)
+{
+	if (!pRoot)
+	{
+		pRoot = pNew;
+
+		return;
+	}
+
+	if (pNew->i <= pRoot->i)
+		InsertToTree(pRoot->pLeft, pNew);
+	else
+		InsertToTree(pRoot->pRight, pNew);
+}
+
+
+int findingMax(Node* root) {
+	if (root == NULL)
+		return 0;
+
+	int left = findingMax(root->pLeft);
+	int right = findingMax(root->pRight);
+
+	int returnMax = max(max(left, right) + root->i, root->i);
+
+	return returnMax;
+}
+
+
+void maxarray(Node* pRoot, vector<int>& maxbranch) {
+
+	if (pRoot->pLeft && findingMax(pRoot->pLeft) > findingMax(pRoot->pRight)) {
+		pRoot = pRoot->pLeft;
+		maxbranch.push_back(pRoot->i);
+		maxarray(pRoot, maxbranch);
+	}
+	if (pRoot->pRight  && findingMax(pRoot->pLeft) < findingMax(pRoot->pRight)){
+		pRoot = pRoot->pRight;
+		maxbranch.push_back(pRoot->i);
+		maxarray(pRoot, maxbranch);
+	}
+
+}
 
 void main()
 {
+	int i;
+
+	Node* pRoot = nullptr;
+	while (true)
+	{
+		cin >> i;
+		if (i == 99)
+			break;
+
+		Node* p = new Node(i);
+		InsertToTree(pRoot, p);
+	}
+	vector<int> maxbranch;
+	
+	maxarray(pRoot, maxbranch);
+	cout << "Branch with the largest sum is: ";
+	cout << pRoot->i << " ";
+	for (int i : maxbranch) {
+		cout << i << " ";
+	}
+	cout << " -> SUM = " << findingMax(pRoot) << endl;
 }
